f75b2265d5b558ca9c5e051e414c470c40d90f9f
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-05-17 22:13:45 +0700

    feat: add zerolog for default logging

--- app/app.go
+++ app/app.go
@@ -1,14 +1,21 @@
 package app
 
 import (
+	"os"
 	"time"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-contrib/logger"
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func RunService() {
 	// this is where the service will run
+
+	// set global logger with zerolog
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stdout}).With().Caller().Logger()
 
 	// set project timezone
 	time.Local = time.FixedZone("Asia/Jakarta", 7*60*60)
@@ -30,6 +37,11 @@ func RunService() {
 		MaxAge:           120 * time.Second,
 	}))
 
+	// set gin middleware for logging
+	r.Use(logger.SetLogger(logger.WithLogger(func(ctx *gin.Context, l zerolog.Logger) zerolog.Logger {
+		return l.Output(zerolog.ConsoleWriter{Out: os.Stdout}).With().Logger()
+	})))
+
 	// set gin middleware for panic hanlder
 	r.Use(gin.Recovery())
 
@@ -39,5 +51,8 @@ func RunService() {
 		})
 	})
 
-	r.Run()
+	err := r.Run()
+	if err != nil {
+		log.Panic().Err(err).Msg("Failed to run service")
+	}
 }
