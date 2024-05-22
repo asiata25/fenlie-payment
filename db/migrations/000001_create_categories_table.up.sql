1ec1c49ece708f6a3387aa40df712346ce3b3ca3
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-05-22 13:09:42 +0700

    docs: rearrange database migration

--- db/migrations/000001_create_categories_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS categories (
-    id uuid DEFAULT uuid_generate_v4() PRIMARY KEY,
-    name varchar(255) NOT NULL DEFAULT '',
-    company_id uuid NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    deleted_at TIMESTAMP NULL
-);
