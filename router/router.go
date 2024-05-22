71a7870e555dcb0701b3388399ac6a3fa0bae701
Mohamad Lutfi Khoirudin <asiatakh25@gmail.com>
2024-05-22 15:46:21 +0700

    Merge: manual merge with feature products

--- router/router.go
+++ router/router.go
@@ -7,6 +7,9 @@ import (
 	companyDelivery "finpro-fenlie/src/company/company_delivery"
 	companyRepository "finpro-fenlie/src/company/company_repository"
 	companyUseCase "finpro-fenlie/src/company/company_use_case"
+	productDelivery "finpro-fenlie/src/product/product_delivery"
+	productRepository "finpro-fenlie/src/product/product_repository"
+	productUseCase "finpro-fenlie/src/product/product_use_case"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
@@ -23,5 +26,10 @@ func InitRouter(v1Group *gin.RouterGroup, db *gorm.DB) {
 	categoryRepo := categoryRepository.NewCategoryRepository(db)
 	categoryUseCase := categoryUseCase.NewCategoryUseCase(categoryRepo)
 	categoryDelivery.NewCategoryDelivery(v1Group, categoryUseCase)
+
+	// Proudct Route
+	productRepo := productRepository.NewProductRepository(db)
+	productUseCase := productUseCase.NewProductUsecase(productRepo)
+	productDelivery.NewProductDelivery(v1Group, productUseCase)
 
 }
